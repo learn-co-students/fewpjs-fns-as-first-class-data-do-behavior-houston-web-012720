e297c12c8bd81ff96e250eb8ec777251b7cd83c8
anamsoomro <anamsoomroed@gmail.com>
2020-03-09 14:35:29 -0500

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  let time_arr = time.split(":")
+  // debugger
+  let hr = parseInt(time_arr[0])
+  if (hr < 12) {
+    return "Good Morning";
+  } else if (hr < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(greeting){
+  let a = document.getElementById("greeting").innerText = greeting
+}
+
